cb019193244d52d247296b3dfe747012d6eae638
colorsugar <1169493556@qq.com>
2026-07-11 03:22:23 +0900

    Complete homepage curve reveal

--- src/components/PaperLayer.tsx
+++ src/components/PaperLayer.tsx
@@ -4,9 +4,14 @@ function PaperCurve() {
   return (
     <div className="paper-curve" aria-hidden="true">
       <svg viewBox="0 0 1440 180" preserveAspectRatio="none">
+        <defs>
+          <clipPath id="paper-stroke-reveal" clipPathUnits="userSpaceOnUse">
+            <rect className="paper-stroke-reveal-rect" x="0" y="0" width="1440" height="180" />
+          </clipPath>
+        </defs>
         <path className="paper-fill" d="M0,90 C360,10 1080,10 1440,90 L1440,180 L0,180 Z" />
         <path className="paper-stroke-base" d="M0,90 C360,10 1080,10 1440,90" />
-        <path className="paper-stroke" pathLength="1" d="M0,90 C360,10 1080,10 1440,90" />
+        <path className="paper-stroke" clipPath="url(#paper-stroke-reveal)" d="M0,90 C360,10 1080,10 1440,90" />
       </svg>
     </div>
   );
